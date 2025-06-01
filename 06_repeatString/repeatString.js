71779263d1005b0d9f122aae0b202d879534ae2a
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2025-06-01 09:48:15 -0400

    looping test

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    str = '';
+    if (num < 0) return 'ERROR';
+    for (let x = 0; x < num; x++) {
+         str += text;
+    }
+    return str;
 };
 
 // Do not edit below this line
