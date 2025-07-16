2ecda7113094b2a90172ffab14c39d49495349f9
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2025-07-16 07:35:02 -0400

    palindrome class

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const reverse = str.replaceAll(" ","").replace(/[^\w\s]|_/g, '').split('').reverse().join('').toLowerCase();
+    const str1 = str.replaceAll(" ","").replace(/[^\w\s]|_/g, '').toLowerCase();
+    if (str1 === reverse)
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line
