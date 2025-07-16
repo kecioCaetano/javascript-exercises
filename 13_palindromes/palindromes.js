a4021939c6ba665f6b670c484a540b9da78cdce0
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2025-07-16 16:53:47 -0400

    fibonacci lesson

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,10 +1,18 @@
 const palindromes = function (str) {
-    const reverse = str.replaceAll(" ","").replace(/[^\w\s]|_/g, '').split('').reverse().join('').toLowerCase();
-    const str1 = str.replaceAll(" ","").replace(/[^\w\s]|_/g, '').toLowerCase();
-    if (str1 === reverse)
-        return true;
-    else
-        return false;
+    
+    const reverse = str.replaceAll(" ","")
+    .replace(/[^\w\s]|_/g, '')
+    .split('')
+    .reverse()
+    .join('')
+    .toLowerCase();
+
+    const str1 = str.replaceAll(" ","")
+    .replace(/[^\w\s]|_/g, '')
+    .toLowerCase();
+    
+    return str1 === reverse
+        
 };
 
 // Do not edit below this line
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(numIteration) {
 
+    if (numIteration <= 0) return "OOPS";
+    if (numIteration === 1) return 1;
+
+    let a = 0
+    let b = 1;
+    
+    for (let i = 2; i <= numIteration; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+    
 };
+
+// > In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+// fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+// fibonacci(6); // returns 8
 
 // Do not edit below this line
 module.exports = fibonacci;
